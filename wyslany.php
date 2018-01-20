0ee43ccb64ddca165b0a797b8919a304ea4ca5c3
Grzegorz Makuch <g.makuch80@gmail.com>
2018-01-20 16:29:19 +0100

    formularz, wstepna wysylka

---
+++ wyslany.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gmaku
+ * Date: 13.01.2018
+ * Time: 12:27
+ */
+include_once 'testowy.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strona - wersja 1.0</title>
+
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/kontakt.css">
+</head>
+<body>
+<header>
+    <div class="header-image"></div>
+</header>
+<nav>
+    <div class="nav-menu-item"><a href="index.php" title="Strona główna">Główna</a></div>
+    <div class="nav-menu-item"><a href="kontakt.html" title="Napisz do mnie">Kontakt</a></div>
+    <div class="nav-menu-item"><a href="galeria.html" title="Galeria">Galeria</a></div>
+    <div class="nav-menu-item"><a href="#" title="Item 4">Item 4</a></div>
+</nav>
+<main>
+    <section class="section-toggle">
+        <?php
+        print("wprowadzono dane:<br>");
+        print("imie: ".$_POST['firstName']."<br>");
+        print("imie: ".$_POST['email']."<br>");
+        print("imie: ".$_POST['city']."<br>");
+        print("imie: ".$_POST['phone']."<br>");
+        print("imie: ".$_POST['msg']."<br>");
+        ?>
+    </section>
+</main>
+<footer>
+    <div class="footer-upper container">
+        <div class="footer-upper-social">
+            <div class="footer-upper-social-box">
+                <a href="#" title="Social 1" target="_blank">
+                    <i class="fa fa-facebook-official" aria-hidden="true"></i>
+                </a>
+            </div>
+            <div class="footer-upper-social-box">
+                <a href="#" title="Social 2" target="_blank">
+                    <i class="fa fa-instagram" aria-hidden="true"></i>
+                </a>
+            </div>
+            <div class="footer-upper-social-box">
+                <a href="#" title="Social 3" target="_blank">
+                    <i class="fa fa-envelope-o" aria-hidden="true"></i>
+                </a>
+            </div>
+        </div>
+        <div class="footer-upper-contact">
+            <div class="footer-upper-contact-box">
+                <p>
+                    <a href="mailto:adres@email.com">email: adres@email.com</a>
+                </p>
+                <p>+48 111 222 333</p>
+            </div>
+        </div>
+    </div>
+    <div class="footer-lower">
+        <div class="footer-lower-info">
+            <p>Projekt i wykonanie MakoskyMqh 2018</p>
+        </div>
+    </div>
+</footer>
+<script src="js/checkForm.js"></script>
+</body>
+</html>
